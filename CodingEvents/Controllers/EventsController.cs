605014fd94bb135268399dc50079e932c1304793
e-bold <enkhmunkh@att.net>
2024-06-03 10:35:09 -0500

    Initial commit

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿namespace CodingEvents;
+using Microsoft.AspNetCore.Mvc;
+
+public class EventsController : Controller
+{
+    private static List<string> Events = new List<string>();
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.events = Events;
+        return View();
+    }
+
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost("/Events/Add")]
+    public IActionResult NewEvent(string name, string Bravo)
+    {
+        Events.Add(name);
+        Events.Add(Bravo);
+
+        return Redirect("/Events");
+    }
+
+}
